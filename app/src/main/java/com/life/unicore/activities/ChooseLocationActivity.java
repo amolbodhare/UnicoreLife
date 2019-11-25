2a490578ea3e1c227169eeea1ae49603c71717dc
vijendrajs <vijendrajs@gmail.com>
2019-11-25 20:20:04 +0530

    Home Activity is created with adapter

--- app/src/main/java/com/life/unicore/activities/ChooseLocationActivity.java
+++ app/src/main/java/com/life/unicore/activities/ChooseLocationActivity.java
@@ -2,6 +2,7 @@ package com.life.unicore.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -14,6 +15,13 @@ public class ChooseLocationActivity extends AppCompatActivity {
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_location);
+        findViewById(R.id.currentLocBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(ChooseLocationActivity.this,HomeActivity.class);
+                startActivity(i);
+            }
+        });
     }
     public void onBackClick(View view) {
         onBackPressed();
--- app/src/main/java/com/life/unicore/activities/WalkThroughActivity.java
+++ app/src/main/java/com/life/unicore/activities/WalkThroughActivity.java
@@ -41,7 +41,7 @@ public class WalkThroughActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         //getWindow().getDecorView().setSystemUiVisibility(0);
-        getWindow().setStatusBarColor(getResources().getColor(R.color.colorAccent));
+        getWindow().setStatusBarColor(getResources().getColor(R.color.transparent));
 
         setContentView(R.layout.activity_walk_through);
 
